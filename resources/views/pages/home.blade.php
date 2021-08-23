9768cdf44b24e91054d124121a6662ab8f97cdaa
symonflores123 <symonjohnflores123@gmail.com>
2021-08-23 12:53:10 +0800

    Trial Push - 3

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,8 +5,7 @@
     <img class="animation__shake" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
 </div>
 @section('content')
-        NEW BRANCH - TEST PUSH 12345
-
+        NEW BRANCH - TEST PUSH 12345 by Symon Flores "Hello Guys"
 @endsection
 
 
